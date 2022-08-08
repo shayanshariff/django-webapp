0eccc7aacb3e02812714f00f1b9c6e249ecf1985
shayanshariff <60033950+shayanshariff@users.noreply.github.com>
2022-08-08 23:47:27 +0500

    templates 1.0

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'ShayanS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 5, 2022'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 8, 2022'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request, "blog/about.html", {'title': 'About'})
